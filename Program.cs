548e52c1a37d709c6fd17c7057e87db78bb5afa6
OftenGuy <77411288+MWB6763@users.noreply.github.com>
2023-02-06 17:11:01 -0500

    Create Program File
    Create Program Class

---
+++ Program.cs
@@ -0,0 +1,24 @@
+/*
+* Created using SharpDevelop.
+* User: OftenGuy
+* Date: February 6, 2023
+* Time: 4:42 PM
+*
+* To change this template use Tools | Options | Coding | Edit Standard Headers.
+*/
+
+using System;
+using System.Windows.Forms;
+namespace SplittenStudio
+{
+    internal sealed class Program
+    {
+        [STAThread]
+        private static void Main(string[] args)
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new SplittenStudio());
+        }
+    }
+}
