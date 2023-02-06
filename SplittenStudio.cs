fa6333e6b763e15d229e3fa8a031e56d303982d3
OftenGuy <77411288+MWB6763@users.noreply.github.com>
2023-02-06 17:07:29 -0500

    Create MainForm Class
    Create main class

---
+++ SplittenStudio.cs
@@ -0,0 +1,27 @@
+/*
+* Created using SharpDevelop.
+* User: OftenGuy
+* Date: February 6, 2023
+* Time: 4:42 PM
+*
+* To change this template use Tools | Options | Coding | Edit Standard Headers.
+*/
+
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+namespace SplittenStudio
+{
+  public partial class MainForm : Form
+  {
+    public MainForm()
+    {
+      InitializeComponent();
+      this.Text = "Splitten Studio";
+    }
+    void MainFormLoad(object sender, EventArgs eventArgs)
+    {
+    }
+  }
+}
