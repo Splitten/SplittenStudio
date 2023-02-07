e27d803ae8bf76712aa81328c523bea1fad62b55
OftenGuy <77411288+MWB6763@users.noreply.github.com>
2023-02-06 21:26:06 -0500

    Add Window Buttons
    Create Window Buttons for MainForm

--- SplittenStudio/MainForm.cs
+++ SplittenStudio/MainForm.cs
@@ -20,7 +20,59 @@ namespace SplittenStudio {
       this.Width = 1280;
       this.Height = 720;
       this.BackColor = Color.FromArgb(150, 150, 150);
+      // this.FormBorderStyle = Style.Windows.Forms.FormBorderStyle.NONE;
+      Button minimizeButton = new Button();
+      this.Controls.Add(minimizeButton);
+      minimizeButton.Text = "-";
+      minimizeButton.Location = new Point(1125, 0);
+      minimizeButton.Size = new Size(50, 35);
+      minimizeButton.Click += new EventHandler(minimizeButtonClicked);
+      minimizeButton.Cursor = Cursors.Hand;
+      ToolTip minimizeToolTip = new ToolTip();
+			minimizeToolTip.AutoPopDelay = 5000;
+			minimizeToolTip.InitialDelay = 1000;
+			minimizeToolTip.ReshowDelay = 500;
+			minimizeToolTip.ShowAlways = true;
+			minimizeToolTip.SetToolTip(minimizeButton, "Minimize");
+      Button maximizeButton = new Button();
+      this.Controls.Add(maximizeButton);
+      maximizeButton.Text = "+";
+      maximizeButton.Location = new Point(1170, 0);
+      maximizeButton.Size = new Size(50, 35);
+      maximizeButton.Click += new EventHandler(maximizeButtonClicked);
+      maximizeButton.Cursor = Cursors.Hand;
+      ToolTip maximizeTooltip = new ToolTip();
+			maximizeToolTip.AutoPopDelay = 5000;
+			maximizeToolTip.InitialDelay = 1000;
+			maximizeToolTip.ReshowDelay = 500;
+			maximizeToolTip.ShowAlways = true;
+			maximizeToolTip.SetToolTip(maximizeButton, "Maximize");
+      Button exitButton = new Button();
+      this.Controls.Add(exitButton);
+      exitButton.Text = "X";
+      exitButton.Location = new Point(1215, 0);
+      exitButton.Size = new Size(50, 35);
+      exitButton.Click += new EventHandler(exitButtonClicked);
+      exitButton.Click = Cursors.Hand;
+      ToolTip exitToolTip = new ToolTip();
+			exitToolTip.AutoPopDelay = 5000;
+			exitToolTip.InitialDelay = 1000;
+			exitToolTip.ReshowDelay = 500;
+			exitToolTip.ShowAlways = true;
+			exitToolTip.SetToolTip(exitButton, "Exit");
     }
     void MainFormLoad(object sender, EventArgs eventArgs) {}
+    void minimizeButtonClicked(object sender, EventArgs eventArgs) {
+        this.WindowState = FormWindowState.Minimized;
+    }
+    void maximizeButtonClicked(object sender, EventArgs eventArgs) {
+        this.WindowState = FormWindowState.Maximized;
+    }
+    void exitButtonClicked(object sender, EventArgs eventArgs) {
+        var button = sender as Button;
+        if(button.Text == "X") {
+            this.Close();
+        }
+    }
   }
 }
