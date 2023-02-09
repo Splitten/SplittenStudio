04dd9ff7e71176307365b356edecd22724b9db56
OftenGuy <77411288+MWB6763@users.noreply.github.com>
2023-02-08 19:14:58 -0500

    Create TitleBarButton File
    Style Title Bar Buttons

---
+++ SplittenStudio/TitleBarButton.cs
@@ -0,0 +1,21 @@
+/*
+ * Created using SharpDevelop.
+ * User: OftenGuy
+ * Date: February 8, 2023
+ * Time: 3:42 PM
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+ using System;
+ using System.Drawing.Drawing2D;
+ using System.Windows.Forms;
+ namespace SplittenStudio {
+    public class TitleBarButtons : Button {
+        protected override void OnPaint(PaintEventArgs e) {
+            var graphicsPath = new GraphicsPath();
+            graphicsPath.AddEllipse(0, 0, ClientSize.Width, ClientSize.Height);
+            this.Region = new System.Drawing.Region(graphicsPath);
+            base.OnPaint(e);
+       }
+    }
+ }
