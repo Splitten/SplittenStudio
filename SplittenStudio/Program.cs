fdca73de713ea946e90e278bc102d7a60be98f09
OftenGuy <77411288+MWB6763@users.noreply.github.com>
2023-02-09 19:23:23 -0500

    Fix Error
    Correct bug on Line 15 where there's no curly bracket (Thanks to my dumb AF eyes for pointing this out)

--- SplittenStudio/Program.cs
+++ SplittenStudio/Program.cs
@@ -6,13 +6,12 @@
 *
 * To change this template use Tools | Options | Coding | Edit Standard Headers.
 */
-
 using System;
 using System.Windows.Forms;
 namespace SplittenStudio {
     internal sealed class Program {
         [STAThread]
-        private static void Main(string[] args) 
+        private static void Main(string[] args)  {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new MainForm());
