29cff41291183b9b847cef2220c11d01e43084e0
OftenGuy <77411288+OftenUser@users.noreply.github.com>
2023-02-12 17:55:15 -0500

    Import Random Things
    Import dumb things just in case "LOL"

--- SplittenStudio/MainForm.cs
+++ SplittenStudio/MainForm.cs
@@ -8,7 +8,14 @@
 */
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
 using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 using System.Windows.Forms;
 namespace SplittenStudio {
   public partial class MainForm : Form {
