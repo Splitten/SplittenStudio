632fee5c58fc4cf4c37b4541ec5b14cff860fbfd
OftenGuy <77411288+OftenUser@users.noreply.github.com>
2023-02-12 17:50:48 -0500

    Rename Logo Class
    Change "LogoButton" To "Logo" and get rid of everything

---
+++ SplittenStudio/Logo.cs
@@ -0,0 +1,17 @@
+/*
+ * Created using SharpDevelop.
+ * User: OftenGuy
+ * Date: February 12, 2023
+ * Time: 3:23 PM
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+ using System;
+ using System.Drawing;
+ using System.Drawing.Drawing2D;
+ using System.Windows.Forms;
+ namespace SplittenStudio {
+    public class Logo : Button {
+        
+    }
+ }
